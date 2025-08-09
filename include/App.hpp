727df41b36748eb9825de917e5dbd4ff8b8e146d
MelchiorRM <melchiorrem001@gmail.com>
2025-08-09 22:11:56 +0800

    Setting up App files...

--- include/App.hpp
+++ include/App.hpp
@@ -1,10 +1,25 @@
 #ifndef APP_HPP
 #define APP_HPP
+#include "include/App.hpp"
+#include "include/User.hpp"
+#include "include/Transaction.hpp"
+#include "include/BudgetManager.hpp"
+#include "include/DBManager.hpp"
 #include <iostream>
+#include <vector>
 using namespace std;
-class App{
+class App {
     private:
+        //vector<User> users;
+        //User* currentUser = nullptr;
     public:
-    //To be implemented...
+        void run();
+        void MainMenu();
+        void AddTransaction();
+        void ViewTransaction();
+        void SetBudget();
+        void Login();
+        void Register();
+        void Logout();
 };
 #endif
--- src/App.cpp
+++ src/App.cpp
@@ -1,4 +1,19 @@
 #include "include/App.hpp"
+#include "include/User.hpp"
+#include "include/Transaction.hpp"
+#include "include/BudgetManager.hpp"
+#include "include/DBManager.hpp"
 #include <iostream>
 using namespace std;
-// To be implemented...
+
+void App::run(){
+    //DBManager database("data/PFT.db");
+    //database.initSchema();
+}
+void MainMenu();
+void AddTransaction();
+void ViewTransaction();
+void SetBudget();
+void Login();
+void Register();
+void Logout();
